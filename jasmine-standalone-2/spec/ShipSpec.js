eefe0e919de06e3574d88defa9f99c2ab74b0d91
HannahHuggins <43819751+HannahHuggins@users.noreply.github.com>
2018-10-08 12:22:08 +0100

    Created a function to dock the ship at the arrival port, and to set sail. We also made tests for both of these functions.

--- jasmine-standalone-2/spec/ShipSpec.js
+++ jasmine-standalone-2/spec/ShipSpec.js
@@ -1,14 +1,38 @@
+// Assigned port, ship, and arrivalPort
 describe ('Ship', function() {
     let port;
     let ship;
+    let arrivalPort;
 
+// Declaring we have a new port, ship, arrivalPor (new objects).
     beforeEach(function() {
         port = new Port();
         ship = new Ship(port);
+        arrivalPort = new Port();
 
     });
 
+    // Running tests -
+    // Expecting the ship's port toBe port 
     it('has a starting port', function() {
+
             expect(ship.getCurrentPort()).toBe(port);
     })
-} );
+    
+    // As the ship is setting sale getCurrentPort is falsy as the ship isn't at a port
+    it('Can set sail from the port', function(){
+        ship.setSail();
+
+        expect(ship.getCurrentPort()).toBeFalsy();
+    
+    })
+
+    // Expecting the ship's currentPort to now be arrivalPort as we are the ship.dock function 
+    it('Can dock at a port', function(){
+        ship.dock(arrivalPort);
+
+        expect(ship.getCurrentPort()).toBe(arrivalPort);
+
+    })
+
+    });
--- jasmine-standalone-2/src/Ship.js
+++ jasmine-standalone-2/src/Ship.js
@@ -1,9 +1,20 @@
+// Created a ship prototype
 Ship.prototype = {
     getCurrentPort: function(){
         return _currentPort
     },
+    // Created a setSail function
+    setSail: function(){
+        _currentPort = null
+    },
+    // Created a dock function
+    dock: function(arrivalPort){
+        _currentPort = arrivalPort
+    }
 }
-
+// Created a ship constructor
 function Ship(port){
     _currentPort = port
+   
+    
 }
